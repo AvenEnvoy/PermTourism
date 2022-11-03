9cdff49f549aaeb7de66e85012f220fbcf2f1c2b
AvenEnvoy <79922111892@ya.ru>
2022-11-04 00:25:12 +0500

    project

--- app/src/main/java/com/bignerdranch/android/permtourism/activities/LoginActivity.kt
+++ app/src/main/java/com/bignerdranch/android/permtourism/activities/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.bignerdranch.android.permtourism.R
 import com.bignerdranch.android.permtourism.databinding.ActivityLoginBinding
+import com.bignerdranch.android.permtourism.fragments.RegFragment
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -12,11 +14,14 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.tvReg.setOnClickListener() {
+            supportFragmentManager.beginTransaction().replace(R.id.flReg, RegFragment.newInstance()).commit()
+        }
     }
 
-    fun onClickSignUp(view: View) {
+    fun onClickSignIp(view: View) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
+
 }
