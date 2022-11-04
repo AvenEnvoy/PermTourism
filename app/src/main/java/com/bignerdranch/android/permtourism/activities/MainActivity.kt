318f582378aa1f99d30d7787b5dcdc26af7e3067
AvenEnvoy <79922111892@ya.ru>
2022-11-05 00:23:16 +0500

    project

--- app/src/main/java/com/bignerdranch/android/permtourism/activities/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/permtourism/activities/MainActivity.kt
@@ -2,11 +2,34 @@ package com.bignerdranch.android.permtourism.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bignerdranch.android.permtourism.R
+import com.bignerdranch.android.permtourism.adapters.PlaceAdapter
+import com.bignerdranch.android.permtourism.databinding.ActivityMainBinding
+import com.bignerdranch.android.permtourism.model.Place
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = PlaceAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
     }
-}
+
+    private fun init() {
+        binding.apply {
+            rvMain.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvMain.adapter = adapter
+            val place = Place(
+                R.drawable.kungur,
+                "Кунгурская пещера",
+                "Одна из самых популярных достопримечательностей Урала. Вместе с Ледяной горой образует историко-природный комплекс регионального значения",
+                57.440953609793006,
+                57.00587191043038,
+                false)
+            adapter.addPlace(place)
+            }
+        }
+    }
---
+++ app/src/main/java/com/bignerdranch/android/permtourism/adapters/PlaceAdapter.kt
@@ -0,0 +1,41 @@
+package com.bignerdranch.android.permtourism.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.permtourism.R
+import com.bignerdranch.android.permtourism.databinding.PlaceItemBinding
+import com.bignerdranch.android.permtourism.model.Place
+
+class PlaceAdapter: RecyclerView.Adapter<PlaceAdapter.PlaceHolder>() {
+    val placeList = ArrayList<Place>()
+    class PlaceHolder(item: View): RecyclerView.ViewHolder(item) {
+        val binding = PlaceItemBinding.bind(item)
+        fun bind(place: Place) {
+            binding.apply {
+                ivPlace.setImageResource(place.imageId)
+                tvTitle.text = place.title
+                tvDesc.text = place.description
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.place_item, parent, false)
+        return PlaceHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PlaceHolder, position: Int) {
+        holder.bind(placeList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return placeList.size
+    }
+
+    fun addPlace(place: Place) {
+        placeList.add(place)
+        notifyDataSetChanged()
+    }
+}
