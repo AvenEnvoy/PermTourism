155c32a5ca50036a5bd877f101907d08ede0b481
AvenEnvoy <79922111892@ya.ru>
2022-11-09 01:07:25 +0500

    project

--- app/src/main/java/com/bignerdranch/android/permtourism/db/UserDB.kt
+++ app/src/main/java/com/bignerdranch/android/permtourism/db/UserDB.kt
@@ -5,12 +5,12 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database (entities = [User::class], version = 1)
+@Database (entities = [User::class], version = 2)
 abstract class UserDB: RoomDatabase() {
     abstract fun getDao(): UserDao
     companion object {
         fun getDB(context: Context): UserDB {
-            return Room.databaseBuilder(context.applicationContext, UserDB::class.java, "UserDB").build()
+            return Room.databaseBuilder(context.applicationContext, UserDB::class.java, "UserDB").fallbackToDestructiveMigration().build()
         }
     }
 }
